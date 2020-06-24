c9de2842c85156cba30bda039c57a8b8daa17881
Jackson Mwaniki <jackson.mwaniki@gmail.com>
2020-06-24 17:49:52 +0300

    add Hero component

--- src/component/AuthorQuiz.js
+++ src/component/AuthorQuiz.js
@@ -2,9 +2,19 @@ import React, {Component} from 'react';
 import '../App.css';
 import 'bootstrap';
 
-class AuthorQuiz extends Component{
+
+function Hero() {
+    return (<div className="row">
+        <div className='jumbotron col-10 offset-1'>
+            <h1>Author Quiz</h1>
+            <p>Select the book written by the author shown.</p>
+        </div>
+    </div>);
+}
+
+class AuthorQuiz extends Component {
     render() {
-        return(
+        return (
             <div className='container-fluid'>
                 <Hero/>
                 <Turn/>
